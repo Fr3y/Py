595fa09c304995086d5afbe5d6ba069da23af761
Frey <74185756+Fr3y@users.noreply.github.com>
2021-01-27 19:19:02 +0530

    Create TAKING_OUTPUT.py

---
+++ TAKING_OUTPUT.py
@@ -0,0 +1,9 @@
+Input
+
+To get input from the user in Python, you can use the intuitively named input function. 
+For example, a game can ask for the user's name and age as input and use them in the game.
+
+The input function prompts the user for input, and returns what they enter as a string (with the contents automatically escaped).
+
+x = input()
+print(x)
